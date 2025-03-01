5e4439ad04b683833e3c46d3f6f399903f0724b8
noteutit <okoroebuka2@gmail.com>
2025-03-01 11:23:15 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -39,4 +39,3 @@ fn main() {
     }
     mod password_generation;
 } 
-
--- src/password_generation.rs
+++ src/password_generation.rs
@@ -2,5 +2,8 @@ use Rnd::Rng;
 use Rnd::distributions::AlphaNumeric;
 
 fn password_generation(length:usize) -> String{
-    let mut random = rand::
+    let mut random = rand::thread_rng();
+    (0..length)
+        .map(|_| rng.Sample(AlphaNumeric))
+        .collect::<String>()
 }
